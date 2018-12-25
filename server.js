df2cae3ee191128fec1fe515f21b61c802e9e4ff
forxinb <>
2018-12-26 01:46:52 +0900

    test

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+// parse setting
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/api', (req, res) => {
+    res.status(200).send({
+        message: "Welcome to CI/CD!",
+        success: true
+    });
+});
+
+app.get('/api/books', (req, res) => {
+    res.status(200).send({
+        books: [{
+            title: "Herry Potter",
+            author: "JK"
+        }, {
+            title: "Book1",
+            author: "JJ"
+        }],
+        success: true
+    });
+});
+
+app.listen(3000, (err) => {
+    console.log('Server listen on port 3000...');
+});
+
+// for testing
+module.exports = app
